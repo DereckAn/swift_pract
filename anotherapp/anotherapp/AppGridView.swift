d256e33ab312683d32aaade9ee53b17d7bf3d032
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-02-01 23:46:59 -0700

    making a grid

---
+++ anotherapp/anotherapp/AppGridView.swift
@@ -0,0 +1,18 @@
+//
+//  AppGridView.swift
+//  anotherapp
+//
+//  Created by Dereck Ángeles on 2/1/24.
+//
+
+import SwiftUI
+
+struct AppGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppGridView()
+}
--- anotherapp/anotherapp/anotherappApp.swift
+++ anotherapp/anotherapp/anotherappApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct anotherappApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppGridView()
         }
     }
 }
