989f52d7f175a23f5cb16caa7ad04c0cf2ad0347
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-02-01 23:24:27 -0700

    first commit

---
+++ anotherapp/anotherapp/anotherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  anotherappApp.swift
+//  anotherapp
+//
+//  Created by Dereck Ángeles on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct anotherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
